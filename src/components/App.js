0945dcc68f11824f0bffe8a480f67f6fbcc4aca8
Mudit Sharma <muditshar001@gmail.com>
2023-06-04 16:09:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,61 @@
-import React, { useEffect, useState } from 'react';
-import '../styles/App.css';
-import SearchBar from './SearchBar';
-import { MoviesList, MovieDetails } from './MoviesList&Detail';
-
+import React, { useEffect, useMemo, useState } from "react";
+import "../styles/App.css";
+import SearchBar from "./SearchBar";
+import { MoviesList, MovieDetails } from "./MoviesList&Detail";
 const App = () => {
-  const [movies, setMovies] = useState([]);
-  const [searchTitle, setSearchTitle] = useState("");
-  const [sortBy, setSortBy] = useState("descending");
-  const [selectedMovie, setSelectedMovie] = useState(null);
+    const [movies, setMovies] = useState([]);
+    const [searchTitle, setSearchTitle] = useState("");
+    const [sortBy, setSortBy] = useState("ascending");
+    const [selectedMovie, setSelectedMovie] = useState(null);
 
-  useEffect(() => {
-    fetchMovies();
-  }, [searchTitle]);
+    // Sorting Movies by Release Year Logic
+    const sortedMovies = useMemo(() => {
+        if (movies.length === 0) {
+            return [];
+        }
+        return movies.sort((firstMovie, secondMovie) => {
+            const firstYear = firstMovie.release_date.split("-")[0];
+            const secondYear = secondMovie.release_date.split("-")[0];
+            const first = parseInt(firstYear);
+            const second = parseInt(secondYear);
+            if (sortBy === "descending") {
+                return first - second;
+            } else {
+                return second - first;
+            }
+        });
+    }, [movies, sortBy]);
+    const handleSort = () => {
+        const sortedList = sortedMovies;
+        console.log(sortedList);
+        setMovies(sortedList);
+        setSortBy((prev) => {
+            if (prev === "ascending") {
+                return "descending";
+            }
+            return "ascending";
+        });
+    };
+    useEffect(() => {
+        console.log("movies modified", movies);
+    }, [movies]);
 
-  const fetchMovies = async () => {
-    try {
-      const API_KEY = '4e44d9029b1270a757cddc766a1bcb63';
-      const response = await fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchTitle}`
-      );
-      const data = await response.json();
-      setMovies(data.results);
-    } catch (error) {
-      console.log('Error fetching movies:', error);
-    }
-  };
-
-  const handleSearch = (searchTerm) => {
-    setSearchTitle(searchTerm);
-  };
-
-  const handleSortClick = () => {
-    setSortBy(sortBy === "descending" ? "ascending" : "descending");
-  };
-
-  const handleMovieClick = (movie) => {
-    setSelectedMovie(movie);
-  };
-
-  const handleCloseMovieDetails = () => {
-    setSelectedMovie(null);
-  };
-
-  return (
-    <div id="main">
-      <h1>Movie Search</h1>
-      <SearchBar onSearch={handleSearch} />
-      <button className="sort-btn" onClick={handleSortClick}>
-        Sort Movies by release year ({sortBy})
-      </button>
-      <MoviesList movies={movies} sortBy={sortBy} onMovieClick={handleMovieClick} />
-      {selectedMovie && (
-        <MovieDetails movie={selectedMovie} onClose={handleCloseMovieDetails} />
-      )}
-    </div>
-  );
+    return (
+        <div id="main">
+            <h1>Movie Search</h1>
+            <SearchBar setMovies={setMovies} />
+            <button onClick={handleSort} className="sort-btn">
+                Sort Movies by release year ({sortBy})
+            </button>
+            <MoviesList setSelectedMovie={setSelectedMovie} movies={movies} />
+            {selectedMovie && (
+                <section className="movie-details">
+                    <MovieDetails
+                        setSelectedMovie={setSelectedMovie}
+                        selectedMovie={selectedMovie}
+                    />
+                </section>
+            )}
+        </div>
+    );
 };
-
-export default App;
